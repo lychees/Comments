53623e77517d4d1f8759e786bfea608efb0308fc
xiaodao <lychees67@gmail.com>
2017-05-05 21:11:39 +0800

    First Contact

---
+++ main.js
@@ -0,0 +1,5 @@
+var txt1="<p>Text.</p>";               // 以 HTML 创建新元素
+var txt2=$("<p></p>").text("Text.");   // 以 jQuery 创建新元素
+var txt3=document.createElement("p");  // 以 DOM 创建新元素
+txt3.innerHTML="Text.";
+$("body").append(txt1,txt2,txt3);         // 追加新元素
