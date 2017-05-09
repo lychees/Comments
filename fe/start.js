5450c2b1b095fd6dc4c7e4d8fc6afd99b2aa8098
wxygeek <wxygeek.abc@gmail.com>
2017-05-09 23:11:22 +0800

    add DOM click handler

--- fe/start.js
+++ fe/start.js
@@ -1,9 +1,12 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  chrome.tabs.executeScript(null, {file: "main.js"});
+$(document).ready(() => {
+  $(document).click((event) => {
+    console.log(event);
+    const newElm = $(`<div>Text.</div>`).appendTo('body');
+    newElm.css({
+      top: event.clientY,
+      left: event.clientX,
+      position: 'absolute',
+      'z-index': 100,
+    });
+  });
 });
