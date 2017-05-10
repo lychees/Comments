bd22e26f104c84196d2584b28e2b01f792c841f0
wxygeek <wxygeek.abc@gmail.com>
2017-05-10 18:06:11 +0800

    add like & close btn

--- fe/start.js
+++ fe/start.js
@@ -1,9 +1,55 @@
-window.addComment = function (content, top, left) {
+window.addComment = function (content, num, top, left) {
   if (content) {
-    const commentElm = $(`<div>${content}</div>`).addClass('comment').appendTo('body');
+    const commentElm = $(`
+    <div>
+      <div class="content">${content}</div>
+      <div class="action">
+        <div class="module">
+          <span class="like" allowClick="true"></span>
+          <span class="num">${num}</span>
+        </div>
+        <div class="module" style="width:15px;">
+          <span class="close"></span>
+        </div>
+      </div>
+    </div>
+    `).addClass('comment').appendTo('body');
     commentElm.css({
       top: top,
       left: left,
+    });
+
+    // 绑定like按钮
+    $(document).on('click', '.like', function () {
+      const likeElm = $(this);
+
+      if (likeElm.attr('allowClick') === 'true') {
+        const numElm = likeElm.next();
+        numElm.html(parseInt(numElm.html(), 10) + 1);
+
+        likeElm.attr('allowClick', 'false');
+        likeElm.css({
+          'background-position': '0 30',
+          cursor: 'default',
+        });
+      }
+    });
+
+    // 绑定close按钮
+    $(document).on('click', '.close', function () {
+      $(this).parent().parent().parent().remove();
+    });
+
+    // like和close的显示隐藏
+    commentElm.mouseenter(() => {
+      commentElm.children('.action').css({
+        display: 'inline-block',
+      });
+    });
+    commentElm.mouseleave(() => {
+      commentElm.children('.action').css({
+        display: 'none',
+      });
     });
   }
 
@@ -22,7 +68,7 @@ $(document).ready(() => {
     inputElm.focus();
     inputElm.bind('keypress', function (event) {
       if (event.keyCode === 13) {
-        window.addComment(inputElm.val(), inputElm.css('top'), inputElm.css('left'));
+        window.addComment(inputElm.val(), 0, inputElm.css('top'), inputElm.css('left'));
         inputElm.remove();
       }
     });
