c327c93ecb0d38be53739010926186c122b54408
Jack Shi <shisoftgenius@gmail.com>
2017-05-15 20:41:52 +0800

    front code

--- fe/start.js
+++ fe/start.js
@@ -1,6 +1,6 @@
 window.addComment = function (content, num, top, left) {
-  if (content) {
-    const commentElm = $(`
+    if (content) {
+        const commentElm = $(`
     <div>
       <div class="content">${content}</div>
       <div class="action">
@@ -14,68 +14,79 @@ window.addComment = function (content, num, top, left) {
       </div>
     </div>
     `).addClass('comment').appendTo('body');
-    commentElm.css({
-      top: top,
-      left: left,
-    });
-
-    commentElm.draggable();
-
-    // 绑定like按钮
-    $(document).on('click', '.like', function () {
-      const likeElm = $(this);
-
-      if (likeElm.attr('allowClick') === 'true') {
-        const numElm = likeElm.next();
-        numElm.html(parseInt(numElm.html(), 10) + 1);
-
-        likeElm.attr('allowClick', 'false');
-        likeElm.css({
-          'background-position': '0 30',
-          cursor: 'default',
+        commentElm.css({
+            top: top,
+            left: left,
         });
-      }
-    });
 
-    // 绑定close按钮
-    $(document).on('click', '.close', function () {
-      $(this).parent().parent().parent().remove();
-    });
+        commentElm.draggable();
 
-    // like和close的显示隐藏
-    commentElm.mouseenter(() => {
-      commentElm.children('.action').css({
-        display: 'inline-block',
-      });
-    });
-    commentElm.mouseleave(() => {
-      commentElm.children('.action').css({
-        display: 'none',
-      });
-    });
-  }
+        // 绑定like按钮
+        $(document).on('click', '.like', function () {
+            const likeElm = $(this);
 
-  // TODO: post new data to api
+            if (likeElm.attr('allowClick') === 'true') {
+                const numElm = likeElm.next();
+                numElm.html(parseInt(numElm.html(), 10) + 1);
+
+                likeElm.attr('allowClick', 'false');
+                likeElm.css({
+                    'background-position': '0 30',
+                    cursor: 'default',
+                });
+            }
+        });
+
+        // 绑定close按钮
+        $(document).on('click', '.close', function () {
+            $(this).parent().parent().parent().remove();
+        });
+
+        // like和close的显示隐藏
+        commentElm.mouseenter(() => {
+            commentElm.children('.action').css({
+                display: 'inline-block',
+            });
+        });
+        commentElm.mouseleave(() => {
+            commentElm.children('.action').css({
+                display: 'none',
+            });
+        });
+    }
+
+    // TODO: post new data to api
 }
 
 $(document).ready(() => {
-
-  // TODO: fetch comments from api
-  $(document).dblclick((event) => {
-    const inputElm = $(`<input type="text"></input>`).addClass('comment-input').appendTo('body');
-    inputElm.css({
-      top: event.pageY,
-      left: event.pageX,
+    const endpoint_url = "http://hq.shisoft.net:8080/comment?url=" + window.encodeURI(window.location.href);
+    $.get(endpoint_url, null, function (res) {
+        $(res).each(function (_, comment) {
+            window.addComment(comment.text, "", comment.y, comment.x);
+        });
+    }, "json");
+    $(document).dblclick((event) => {
+        const inputElm = $(`<input type="text"></input>`).addClass('comment-input').appendTo('body');
+        inputElm.css({
+            top: event.pageY,
+            left: event.pageX,
+        });
+        inputElm.focus();
+        inputElm.bind('keypress', function (event) {
+            if (event.keyCode === 13) {
+                $.post(endpoint_url, {
+                    author: "",
+                    text: comment.text,
+                    x: comment.x,
+                    y: comment.y
+                }, function (res) {
+                    window.addComment(inputElm.val(), 0, inputElm.css('top'), inputElm.css('left'));
+                    inputElm.remove();
+                }, "json");
+            }
+        });
+        inputElm.focusout(() => {
+            inputElm.remove();
+        });
     });
-    inputElm.focus();
-    inputElm.bind('keypress', function (event) {
-      if (event.keyCode === 13) {
-        window.addComment(inputElm.val(), 0, inputElm.css('top'), inputElm.css('left'));
-        inputElm.remove();
-      }
-    });
-    inputElm.focusout(() => {
-      inputElm.remove();
-    });
-  });
 });
